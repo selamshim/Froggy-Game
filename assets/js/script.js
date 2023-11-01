8d5886429598ee14ecf8da886159c3234c54d8a9
selamshim <shimeselam@gmail.com>
2023-11-01 12:38:44 +0100

    favicon and js file added to move toady

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+const toady = document.querySelector("#toady")
+const theme = new Audio
+theme.src = "assets/audio/LilyIslands.mp3"
+
+const position= {
+    x:0,
+    y:0,
+}
+
+//key down keep moving while in key up it must be touched everry time
+
+document.addEventListener("keydown", (event) =>{
+    //audio will play when key is pressed
+    theme.play()
+    switch(event.key){
+        //ArrowUp listen upward arrow
+        case "ArrowUp":
+            //change the position by 10 to make faster
+            position.y -= 10
+            break
+        //ArrowDown listen the downward arrow    
+        case "ArrowDown":
+            position.y += 10
+            break
+        //ArrowLeft listen the left arrow    
+        case "ArrowLeft":
+            position.x -= 10
+            break
+        //ArrowRight listen the right arrow    
+        case "ArrowRight":
+            position.x += 10
+            break    
+    }
+
+    toady.style.trransform= `translate(
+        ${position.x}px,
+        ${position.y}px
+    )`
+
+
+})
