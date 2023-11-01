d4e7c576c3d9dea3f62743c6d11ca95c9e8c5b48
selamshim <shimeselam@gmail.com>
2023-11-01 13:02:27 +0100

    toady transform position

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,41 @@
-const toady = document.querySelector("#toady")
-const theme = new Audio
-theme.src = "assets/audio/LilyIslands.mp3"
+let toady = document.getElementById('toady');
+let theme = new Audio();
+theme.src = "assets/audio/LilyIslands.mp3";
 
-const position= {
+let position= {
     x:0,
     y:0,
-}
+};
 
 //key down keep moving while in key up it must be touched everry time
 
 document.addEventListener("keydown", (event) =>{
     //audio will play when key is pressed
-    theme.play()
+    theme.play();
     switch(event.key){
         //ArrowUp listen upward arrow
         case "ArrowUp":
             //change the position by 10 to make faster
-            position.y -= 10
-            break
+            position.y -= 10;
+            break;
         //ArrowDown listen the downward arrow    
         case "ArrowDown":
-            position.y += 10
-            break
+            position.y += 10;
+            break;
         //ArrowLeft listen the left arrow    
         case "ArrowLeft":
-            position.x -= 10
-            break
+            position.x -= 10;
+            break;
         //ArrowRight listen the right arrow    
         case "ArrowRight":
-            position.x += 10
-            break    
+            position.x += 10;
+            break;
     }
 
-    toady.style.trransform= `translate(
+    toady.style.transform= `translate(
         ${position.x}px,
         ${position.y}px
     )`
 
 
-})
+});
